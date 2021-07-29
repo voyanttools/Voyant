5fe0477df07a056692636ec6e504dc1fbfdc6c15
Andrew James <ajmacdonald@users.noreply.github.com>
2021-07-29 10:28:26 -0400

    spyral: text editor ux improvements

--- src/main/webapp/app/notebook/editor/TextEditor.js
+++ src/main/webapp/app/notebook/editor/TextEditor.js
@@ -1,12 +1,14 @@
-Ext.define("Voyant.notebook.editor.TextEditor", {
-	extend: "Ext.Component",
-	mixins: ["Voyant.util.Localization"],
-	alias: "widget.notebooktexteditor",
+Ext.define('Voyant.notebook.editor.TextEditor', {
+	extend: 'Ext.Component',
+	mixins: ['Voyant.util.Localization'],
+	alias: 'widget.notebooktexteditor',
 	cls: 'notebook-text-editor',
 	config: {
 		editor: undefined,
 		isEditRegistered: false,
-		currentHeight: 0
+		currentHeight: 0,
+		isFresh: undefined,
+		isEnabled: false
 	},
 	statics: {
 		i18n: {
@@ -14,98 +16,116 @@ Ext.define("Voyant.notebook.editor.TextEditor", {
 	},
 	border: false,
 	constructor: function(config) {
+		config.isFresh = config.content ? false : true;
 		Ext.apply(this, {
-			html: config.content ? config.content : this.localize("emptyText")
+			html: config.content ? config.content : '<p style="text-align: center;">'+this.localize('emptyText')+'</p>'
 		});
         this.callParent(arguments);
 	},
 	listeners: {
 		boxready: function(cmp) {
-			this.getTargetEl().on("click", function(e, t) {
-				if (t.tagName !== "A") {
-					this.handleClick(cmp);
+			this.ownerCt.getTargetEl().on('click', function(e, t) {
+				if (t.tagName !== 'A') {
+					this.handleClick();
 				}
 			}, this);
 		},
 		removed: function(cmp, container) {
 			// properly remove editor
-			if (cmp.getEditor()) {
-				cmp.getEditor().focusManager.blur(true); //focusManager bug workaround, see: https://dev.ckeditor.com/ticket/16825
-				cmp.getEditor().destroy();
+			if (this.getEditor()) {
+				this.getEditor().focusManager.blur(true); //focusManager bug workaround, see: https://dev.ckeditor.com/ticket/16825
+				this.getEditor().destroy();
 			}
 		}
 	},
 	
-	handleClick: function(cmp) {
-		if (!cmp.getEditor() || (cmp.getEditor() && cmp.getEditor().readOnly)) {
-			var el = this.getTargetEl();
-			el.set({contenteditable: true});
-			this.findParentByType("notebookeditorwrapper").setIsEditing(true);
-			if (!cmp.getEditor()) {
-				// when upgrading ckeditor, remember to copy stopediting and inserthtml4x plugins
-				var editor = CKEDITOR.inline(el.dom, {
-					toolbar: [
-						{ name: 'basicstyles', items: [ 'Bold', 'Italic', '-', 'RemoveFormat' ] },
-						{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Justify', 'Outdent', 'Indent', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
-						{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
-						{ name: 'styles', items: [ 'Styles', 'Format' ] },
-						{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor'] },
-						{ name: 'insert', items: [ 'Image', 'Table' ] },
-						{ name: 'document', items: [ 'inserthtml4x', 'Sourcedialog', 'Stopediting'] }
-					],
-					
-					extraPlugins: 'stopediting,sourcedialog,justify,colorbutton,inserthtml4x',
-					allowedContent: true,
-					toolbarCanCollapse: false,
-					startupFocus: true
-				});
-				
-				// erase contents if it's click to edit (not localized, FIXME
-				editor.on("focus", function(evt) {
-					if (this.getTargetEl().dom.innerText==this.localize("emptyText")) {
-						this.getTargetEl().update('')
-					}
-				}, this)
-				
-				editor.on('contentDom', function() {
-					var editable = editor.editable();
-					editable.attachListener(editable, 'click', function(evt) {
-						// prevent further listeners from firing, esp. the one that prevents links from working ( https://stackoverflow.com/a/45616460 )
-						evt.stop();
-					}, null, null, 0); // zero (i.e. very high) priority
-				})
-				
-				editor.on("blur", function(evt) {
-//					cmp.setEditor(undefined);
-					cmp.findParentByType("notebookeditorwrapper").setIsEditing(false).getEl().fireEvent("mouseout");
-//					el.set({contenteditable: false});
-					editor.setReadOnly(true);
-				})
-				editor.on("change", function() {
-					var editorHeight = editor.container.$.clientHeight;
-					if (editorHeight !== this.getCurrentHeight()) {
-						this.findParentByType("notebookeditorwrapper").setHeight(editorHeight);
-						this.setCurrentHeight(editor.container.$.clientHeight)
-					}
-					if (!this.getIsEditRegistered()) {
-						this.findParentByType("notebook").setIsEdited(true);
-						this.setIsEditRegistered(true);
-
-					} else {
-						var me = this; // make sure to allow edits to be auto-saved every 30 seconds
-						setTimeout(function() {
-							me.setIsEditRegistered(false);
-						}, 30000);
-					}
-				}, this)
-				this.setEditor(editor);
-			} else {
-				cmp.getEditor().setReadOnly(false);
+	handleClick: function() {
+		if (!this.getEditor()) {
+			this._initEditor();
+		} else {
+			if (this.getIsEnabled() === false) {
+				this._enable();
 			}
 		}
 	},
 	
 	getContent: function() {
 		return this.getTargetEl().dom.innerHTML;
+	},
+
+	_initEditor: function() {
+		var el = this.getTargetEl();
+		// when upgrading ckeditor, remember to copy stopediting and inserthtml4x plugins
+		var editor = CKEDITOR.inline(el.dom, {
+			toolbar: [
+				{ name: 'basicstyles', items: [ 'Bold', 'Italic', '-', 'RemoveFormat' ] },
+				{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'Justify', 'Outdent', 'Indent', 'Blockquote', 'JustifyLeft', 'JustifyCenter', 'JustifyRight' ] },
+				{ name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+				{ name: 'styles', items: [ 'Styles', 'Format' ] },
+				{ name: 'links', items: [ 'Link', 'Unlink', 'Anchor'] },
+				{ name: 'insert', items: [ 'Image', 'Table' ] },
+				{ name: 'document', items: [ /*'inserthtml4x',*/ 'Sourcedialog', /*'Stopediting'*/] }
+			],
+			
+			extraPlugins: 'sourcedialog,justify,colorbutton',//,stopediting,inserthtml4x',
+			allowedContent: true,
+			toolbarCanCollapse: false,
+			startupFocus: 'end'
+		});
+		
+		editor.on('contentDom', function() {
+			this._enable();
+			var editable = editor.editable();
+			editable.attachListener(editable, 'click', function(evt) {
+				// prevent further listeners from firing, esp. the one that prevents links from working ( https://stackoverflow.com/a/45616460 )
+				evt.stop();
+			}, null, null, 0); // zero (i.e. very high) priority
+		}, this);
+
+		editor.on('focus', function(evt) {
+			if (this.getIsFresh()) {
+				this.setIsFresh(false);
+				this.getTargetEl().update('<p></p>'); // insert paragraphs because otherwise ckeditor will do it automatically
+			}
+		}, this);
+		
+		editor.on('blur', function(evt) {
+			this._disable();
+		}, this);
+
+		editor.on('change', function() {
+			var editorHeight = editor.container.$.clientHeight;
+			if (editorHeight !== this.getCurrentHeight()) {
+				this.findParentByType('notebookeditorwrapper').setHeight(editorHeight);
+				this.setCurrentHeight(editor.container.$.clientHeight)
+			}
+			if (!this.getIsEditRegistered()) {
+				this.findParentByType('notebook').setIsEdited(true);
+				this.setIsEditRegistered(true);
+
+			} else {
+				var me = this; // make sure to allow edits to be auto-saved every 30 seconds
+				setTimeout(function() {
+					me.setIsEditRegistered(false);
+				}, 30000);
+			}
+		}, this);
+
+		this.setEditor(editor);
+	},
+
+	_enable: function() {
+		this.getEditor().setReadOnly(false);
+		this.getEditor().focus();
+
+		this.findParentByType('notebookeditorwrapper').setIsEditing(true);
+		this.setIsEnabled(true);
+	},
+
+	_disable: function() {
+		this.getEditor().setReadOnly(true);
+
+		this.findParentByType('notebookeditorwrapper').setIsEditing(false);
+		this.setIsEnabled(false);
 	}
 })
