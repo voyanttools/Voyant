61e56e595096eeacd72a8be840cb11bfaa336902
Voyant Tools <voyanttools@users.noreply.github.com>
2022-11-18 20:59:30 +0000

    chore: add JS build files

--- src/main/webapp/resources/voyant/current/voyant.js
+++ src/main/webapp/resources/voyant/current/voyant.js
@@ -1,4 +1,4 @@
-/* This file created by JSCacher. Last modified: Tue Nov 01 20:48:11 UTC 2022 */
+/* This file created by JSCacher. Last modified: Fri Nov 18 20:59:22 UTC 2022 */
 function Bubblelines(config) {
 	this.container = config.container;
 	this.externalClickHandler = config.clickHandler;
@@ -31469,7 +31469,7 @@ Ext.define('Voyant.panel.Trends', {
     },
     
     loadCorpusTerms: function(params) {
-    	// if our corpus only has one document or if we have docIndex d
+    	// if our corpus only has one document or if we have docIndex defined
     	if (
     		this.getCorpus().getDocumentsCount()<2 || // only one document
     		this.getApiParam("mode")=="document" || // in document mode
@@ -31480,7 +31480,7 @@ Ext.define('Voyant.panel.Trends', {
     	if (!this.getApiParam("query")) {
         	this.getCorpus().getCorpusTerms().load({
         		params: {
-        			limit: 5,
+        			limit: this.getApiParam('limit'),
         			stopList: this.getApiParam("stopList")
         		},
         		callback: function(records, operation, success) {
@@ -31507,7 +31507,12 @@ Ext.define('Voyant.panel.Trends', {
 			limit: 100, // should have query, so no limit 
 	    	stopList: "" // automatic queries should be stopped already
     	});
-    	var docLabels = this.getCorpus().map(function(doc) {return doc.getTinyTitle()})
+    	var docLabels = [];
+		var docLabelsFull = [];
+		this.getCorpus().each(function(doc) {
+			docLabels.push(doc.getTinyTitle());
+			docLabelsFull.push(doc.getTitle());
+		})
     	if (Ext.Array.unique(docLabels).length<docLabels.length) { // we have duplicates, add index
     		docLabels = docLabels.map(function(doc,i) {return (i+1)+")"+ doc})
     	}
@@ -31517,11 +31522,11 @@ Ext.define('Voyant.panel.Trends', {
     		callback: function(records, operation, success) {
     			var data = [], series = [], chartType = this.getApiParam('chartType');
     			records.forEach(function(record, index) {
-    	    		var term = record.get('term'), docIndex = record.get("docIndex");
+    	    		var term = record.get('term');
     	    		var color = this.getApplication().getColorForTerm(term, true);
     	    		record.get('distributions').forEach(function(r, i) {
     	    			if (!data[i]) {
-    	    				data[i] = {"index": docLabels[i]};
+    	    				data[i] = {"index": docLabels[i], "docTitle": docLabelsFull[i]};
     	    			}
     	    			data[i]["_"+index] = withDistributions=='relative' ? r.toFixed(7) : r;
     	    			data[i]["term"+index] = term;
@@ -31593,35 +31598,13 @@ Ext.define('Voyant.panel.Trends', {
     		scope: this
     	});
   
-    },
-    
-    getItemToolTip: function (toolTip, record, ctx) {
-    	
-    	var parts = ctx.field.split("_"),
-    		docIndex = parts.length==2 ? ctx.index : parts[2],
-    		pos = parseInt(parts[1]),
-    		title = ctx.series.getTitle(),
-    		term = Ext.isArray(title) ? title[pos] : title,
-    		colors = ctx.series.getColors(),
-    		color = colors.length==1 ? colors[0] : colors[pos];
-        var html = "<span class='x-legend-item-marker' style='background:"+color+
-        		"; left: 2px;'></span> <span style='padding-left: 1.2em; font-weight: bold;'>"+
-        		term+"</span>: "+record.get(ctx.field)+
-    			"<br/><i>"+this.getCorpus().getDocument(docIndex).getShortTitle()+"</i>"
-		if (this.getApiParam("mode")=="corpus") {
-    		html+="<div style='font-size: smaller'>"+this.localize('dblClickItem')
-		} else {
-			html+="<br/>"+this.localize('segment')+" "+(ctx.index+1)
-		}
-    	toolTip.setHtml(html);
-    },
-    
+    },   
 
     loadDocumentTerms: function(params) {
     	if (!this.getApiParam("query")) {
         	this.getCorpus().getCorpusTerms().load({
         		params: {
-        			limit: this.getCorpus().getDocumentsCount()<2 ? 5 : 2,
+        			limit: this.getApiParam('limit'),
         			stopList: this.getApiParam("stopList")
         		},
         		callback: function(records, operation, success) {
@@ -31641,7 +31624,6 @@ Ext.define('Voyant.panel.Trends', {
 			sort: 'termasc',
 			stopList: undefined
     	});
-    	var docLabels = this.getCorpus().map(function(doc) {return doc.getTinyTitle()})
     	var singleDoc;
     	if (this.getCorpus().getDocumentsCount()==1) {
     		singleDoc=this.getCorpus().getDocument(0)
@@ -31667,10 +31649,9 @@ Ext.define('Voyant.panel.Trends', {
     	    		var term = record.get('term');
     	    		var docIndex = record.get('docIndex');
     	    		var color = singleDoc ? this.getApplication().getColorForTerm(term, true) : this.getApplication().getColor(docIndex, true);
-    	    		var docIndex = record.get('docIndex');
     	    		record.get('distributions').forEach(function(r, i) {
     	    			if (!data[i]) {
-    	    				data[i] = {docIndex: docIndex, index: (i+1)};
+    	    				data[i] = {index: (i+1)};
     	    			}
     	    			data[i]["_"+index+"_"+docIndex] = withDistributions=='relative' ? r.toFixed(7) : r;
     	    			data[i]["term"+index] = term;
@@ -31735,7 +31716,7 @@ Ext.define('Voyant.panel.Trends', {
         				type: 'category',
                 		position: 'bottom',
                 		title: {
-                			text: this.localize("segmentsTitle") + (singleDoc ? " ("+singleDoc.getShortTitle()+")" : "")
+                			text: this.localize("segmentsTitle") + (singleDoc ? " ("+singleDoc.getTitle()+")" : "")
                 		}
                 		
         			}]
@@ -31745,7 +31726,26 @@ Ext.define('Voyant.panel.Trends', {
     	});
   
     },
-
+	    
+    getItemToolTip: function (toolTip, record, ctx) {
+    	var parts = ctx.field.split("_"),
+    		docIndex = parts.length==2 ? ctx.index : parts[2],
+    		pos = parseInt(parts[1]),
+    		title = ctx.series.getTitle(),
+    		term = Ext.isArray(title) ? title[pos] : title,
+    		colors = ctx.series.getColors(),
+    		color = colors.length==1 ? colors[0] : colors[pos];
+        var html = "<span class='x-legend-item-marker' style='background:"+color+
+        		"; left: 2px;'></span> <span style='padding-left: 1.2em; font-weight: bold;'>"+
+        		term+"</span>: "+record.get(ctx.field)+
+    			"<br/><i>"+this.getCorpus().getDocument(docIndex).getShortTitle()+"</i>"
+		if (this.getApiParam("mode")=="corpus") {
+    		html+="<div style='font-size: smaller'>"+this.localize('dblClickItem')
+		} else {
+			html+="<br/>"+this.localize('segment')+" "+(ctx.index+1)
+		}
+    	toolTip.setHtml(html);
+    },
     
     buildChart: function(config) {
     	var chartType = this.getApiParam('chartType'), labels = false;
@@ -31792,15 +31792,15 @@ Ext.define('Voyant.panel.Trends', {
             			}, 1000, this);
                 		if (this.getApiParam("mode")=="document") {
                 			var parts = item.field.split("_"),
-                				docIndex = parts[2],
+                				docIndex = parseInt(parts[2]),
                 				doc = this.getCorpus().getDocument(docIndex),
                 				tokens = doc.get('tokensCount-lexical'),
                 				position = parseInt(item.index  * tokens / parseInt(this.getApiParam("bins")))
                 			this.dispatchEvent("documentIndexTermsClicked", this, [{
                     			term: item.series.term,
-                    			docIndex: parts[2],
+                    			docIndex: docIndex,
                     			position: position
-                    		}]);                			
+                    		}]);
                 		} else {
                 			if (this.clickTimer) {clearTimeout(this.clickTimer);}
                 			var me = this;
@@ -31988,7 +31988,12 @@ Ext.define('Voyant.panel.Trends', {
 
 		store.each(function(model) {
 			data = model.getData();
-			var entry = [data['index']];
+			var entry = [];
+			if (data['docTitle'] !== undefined) {
+				entry.push(data['docTitle']);
+			} else {
+				entry.push(data['index']);
+			}
 			valueKeys.forEach(function(valueKey) {
 				entry.push(data[valueKey]);
 			});
@@ -40797,7 +40802,7 @@ Ext.define('Voyant.VoyantApp', {
     	},
     	api: {
 			palette: 'default',
-			categories: undefined,
+			categories: 'auto',
     		lang: undefined,
     		debug: undefined
     	}
@@ -41036,7 +41041,7 @@ Ext.define('Voyant.VoyantCorpusApp', {
     	i18n: {
     	},
     	api: {
-    		toolFlow: undefined
+    		toolFlow: undefined // TODO what is this supposed to do?
     	}
     },
     
