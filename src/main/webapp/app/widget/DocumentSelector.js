909dd7e741b0c929692f98d442fb8f9036640e04
Andrew MacDonald <ajmacdonald@users.noreply.github.com>
2024-03-19 14:35:05 -0400

    fix(documentselector): make checked docs reflect api params

--- src/main/webapp/app/widget/DocumentSelector.js
+++ src/main/webapp/app/widget/DocumentSelector.js
@@ -117,6 +117,23 @@ Ext.define('Voyant.widget.DocumentSelector', {
     populate: function(docs) {
     	this.setDocs(docs);
     	
+		var checkedDocs = {docId: undefined, docIndex: undefined, match: false};
+		var panel = this.findParentBy(function(clz) {
+			return clz.mixins["Voyant.panel.Panel"];
+		});
+		if (panel && panel.getApiParam) {
+			checkedDocs.docId = panel.getApiParam('docId');
+			checkedDocs.docIndex = panel.getApiParam('docIndex');
+			if (checkedDocs.docId !== undefined && typeof checkedDocs.docId === 'string') {
+				checkedDocs.docId = checkedDocs.docId.split(',');
+				checkedDocs.match = true;
+			}
+			if (checkedDocs.docIndex !== undefined && typeof checkedDocs.docIndex === 'string') {
+				checkedDocs.docIndex = checkedDocs.docIndex.split(',').map(function(di) { return parseInt(di)});
+				checkedDocs.match = true;
+			}
+		}
+
     	var menu = this.getMenu();
     	menu.removeAll();
     	
@@ -124,11 +141,15 @@ Ext.define('Voyant.widget.DocumentSelector', {
     	
     	var groupId = 'docGroup'+Ext.id();
     	docs.forEach(function(doc, index) {
+			var checked = isSingleSelect && index == 0 || !isSingleSelect;
+			if (checkedDocs.match) {
+				checked = (checkedDocs.docId !== undefined && checkedDocs.docId.indexOf(doc.get('id')) !== -1) || (checkedDocs.docIndex !== undefined && checkedDocs.docIndex.indexOf(doc.get('index')) !== -1);
+			}
     		menu.add({
     			xtype: 'menucheckitem',
     			text: doc.getShortTitle(),
     			docId: doc.get('id'),
-    			checked: isSingleSelect && index == 0 || !isSingleSelect,
+    			checked: checked,
     			group: isSingleSelect ? groupId : undefined,
     			checkHandler: function(item, checked) {
     				if (this.getSingleSelect() && checked) {
