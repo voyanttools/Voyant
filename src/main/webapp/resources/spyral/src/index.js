3c1f4d803871dd6dd6a17df6a70d27c443287712
Andrew MacDonald <ajmacdonald@users.noreply.github.com>
2025-01-07 17:23:38 -0500

    feat(spyral): add assign to Promise

--- src/main/webapp/resources/spyral/src/index.js
+++ src/main/webapp/resources/spyral/src/index.js
@@ -27,6 +27,15 @@ const Spyral = {
 	Categories
 };
 
+/**
+ * Assign the result of a Promise to a new or existing global variable.
+ * @memberof Promise
+ * @param {String} variableName The name of the variable to which the result of the Promise should be assigned.
+ * @returns {*} The result of the Promise
+ */
+Promise.prototype.assign = function(variableName) {
+	return this.then(promiseResult => {window[variableName] = promiseResult; return promiseResult;});
+}
 
 /**
  * @class window
