192e79d588074595a5acb68516dd8f25ed5e9121
Andrew MacDonald <ajmacdonald@users.noreply.github.com>
2025-01-10 16:06:03 -0500

    fix(spyral): fix showError to use changed show method

--- src/main/webapp/resources/spyral/src/show.js
+++ src/main/webapp/resources/spyral/src/show.js
@@ -7,53 +7,46 @@
  * @param {Object} config A config object containing attributes to add to the containing element.
  */
 function show(contents, config = {}) {
-	if (this && this.then) {
-		var arg = contents;
-		this.then(function(val) {
-			show.call(val, val, arg);
-		})
-	} else {
-		if (contents === undefined) {
-			return;
-		}
+	if (contents === undefined) {
+		return;
+	}
 
-		if (document.querySelector('.spyral-dv-container') !== null) {
-			document.querySelector('.spyral-dv-container').remove(); // get rid of dataviewer if it exists
-		}
-		
-		if (contents.constructor === Object || Array.isArray(contents)) {
-			return contents; // it's JSON so use the dataviewer
-		}
+	if (document.querySelector('.spyral-dv-container') !== null) {
+		document.querySelector('.spyral-dv-container').remove(); // get rid of dataviewer if it exists
+	}
+	
+	if (contents.constructor === Object || Array.isArray(contents)) {
+		return contents; // it's JSON so use the dataviewer
+	}
 
-		if (contents instanceof Node) {
-			if (contents instanceof Element) {
-				contents = contents.outerHTML;
-			} else if ((contents instanceof Document || contents instanceof DocumentFragment) && contents.firstElementChild !== null) {
-				if (contents.body) {
-					contents = contents.body;
-				}
-				contents = contents.firstElementChild.outerHTML;
+	if (contents instanceof Node) {
+		if (contents instanceof Element) {
+			contents = contents.outerHTML;
+		} else if ((contents instanceof Document || contents instanceof DocumentFragment) && contents.firstElementChild !== null) {
+			if (contents.body) {
+				contents = contents.body;
 			}
+			contents = contents.firstElementChild.outerHTML;
 		}
-		if (contents.then) { // check if we currently have a promise
-			return contents.then(function(text) {show(text, len)})
-		}
-		if (contents.toHtml) {contents=contents.toHtml()}
-		else if (contents.getString) {contents=contents.getString()}
-		else if (contents.toString) {contents=contents.toString()}
+	}
+	if (contents.then) { // check if we currently have a promise
+		return contents.then(function(text) {show(text, config)})
+	}
+	if (contents.toHtml) {contents=contents.toHtml()}
+	else if (contents.getString) {contents=contents.getString()}
+	else if (contents.toString) {contents=contents.toString()}
 
-		if (contents.then) { // check again to see if we have a promise (like from toString())
-			contents.then(function(text) {show(text, len)})
-		} else {
-			if (config['class'] === undefined) config['class'] = "info";
-			var html = "<div";
-			Object.keys(config).forEach(key => {
-				var value = config[key];
-				html += ` ${key}="${value}"`;
-			})
-			html += `>${contents}</div>`;
-			document.body.insertAdjacentHTML('beforeend', html);
-		}
+	if (contents.then) { // check again to see if we have a promise (like from toString())
+		contents.then(function(text) {show(text, config)})
+	} else {
+		if (config['class'] === undefined) config['class'] = "info";
+		var html = "<div";
+		Object.keys(config).forEach(key => {
+			var value = config[key];
+			html += ` ${key}="${value}"`;
+		})
+		html += `>${contents}</div>`;
+		document.body.insertAdjacentHTML('beforeend', html);
 	}
 }
 
@@ -83,7 +76,7 @@ function showError(error, more) {
 		var encodedMore = more.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;').replace('"', '&quot;').replace("'", '&apos;')
 		error='<strong>'+error.toString()+'</strong><pre><span style="cursor:pointer;text-decoration:underline;" onclick="this.nextElementSibling.style.display=\'block\';this.style.display=\'none\';">Details</span><span style="display:none;">'+encodedMore+'</span></pre>';
 	}
-	show(error, undefined, 'error');
+	show(error, {class: 'error'});
 }
 
 export {show, showError};
