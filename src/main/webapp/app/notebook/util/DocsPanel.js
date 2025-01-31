29ce10e8775fda4371f17cd5e157126fafe303e2
Andrew MacDonald <ajmacdonald@users.noreply.github.com>
2025-01-31 18:15:17 -0500

    fix(docs): work on typedefs and Tools

--- src/main/webapp/app/notebook/util/DocsPanel.js
+++ src/main/webapp/app/notebook/util/DocsPanel.js
@@ -16,10 +16,10 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 				'<a href="https://voyant-tools.org/spyral/learnspyral@gh/Tutorials/">Tutorials</a> - Link to tutorial notebooks.<br>' +
 				'<a href="/docs/Spyral.Corpus.html#method-tool">Tools Documentation</a> - Full Tools Documentation.<br>' +
 				'<br>' +
-				'Alternativly take a look at our detailed Documentation:<br>' +
+				'Alternatively take a look at our detailed Documentation:<br>' +
 				'<br>' +
-				'<a href="/docs">Full API Documentation</a> - Open up detailed API Documentation<br>' +
-				'<a href="/docs/tools">Full Tool Documentation</a> - Open up detailed Tool Documentation<br><hr>',
+				'<a href="/docs" rel="help">Full API Documentation</a> - Open up detailed API Documentation<br>' +
+				'<a href="/docs/Tools.html" rel="help">Full Tool Documentation</a> - Open up detailed Tool Documentation<br><hr>',
 			outlineApi: 'Here is a list of the Spyral classes that can be used in your notebook:',
 			loadingDocs: 'Loading Docs'
 		}
@@ -28,81 +28,80 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 	lastDocEntryClass: undefined,
 	lastDocEntryMethod: undefined,
 
-	classTemplate: new Ext.XTemplate(
-		'<div>',
-			'<div class="doc-contents">{overview}</div>',
-			'<div class="members">',
-			'<div class="members-section">',
-				'<h3 class="members-title icon-method">Type Definitions</h3>',
-				'<tpl for="typedefs">',
-				'<div class="subsection">',
-					'<h4 class="members-subtitle">{memberof}~{name}</h4>',
-					'<tpl for="props">',
-						'<div id="{[parent.memberof.replace(".","_")]}-{parent.name}-{name}" class="member">',
-							'<div class="title">',
-								'<a href="/docs/{[parent.memberof.replace(".","_")]}-{parent.name}-{name}" class="name">{name}</a>',
-								'( <span class="pre">',
-								'<tpl for="type">{.}{[xindex < xcount ? "|" : ""]}</tpl>',
-								'</span> )',
-							'</div>',
-							'<div class="description">',
-								'<div class="long">{desc}</div>',
-							'</div>',
-						'</div>',
+	typedefsTemplate: new Ext.XTemplate(
+		'<div class="subsection">',
+			'<h4 class="members-subtitle">{memberof}~{name}</h4>',
+			'<tpl for="props">',
+				'<div id="{[parent.memberof.replace(".","_")]}-{parent.name}-{name}" class="member">',
+					'<div class="title">',
+						'<a href="/docs/{[parent.memberof.replace(".","_")]}-{parent.name}-{name}" class="name">{name}</a>',
+						' : <span class="pre">',
+						'<tpl for="type">{.}{[xindex < xcount ? "|" : ""]}</tpl>',
+						'</span>',
+					'</div>',
+					'<div class="description">',
+						'<div class="long">{desc}</div>',
+					'</div>',
+				'</div>',
+			'</tpl>',
+		'</div>',
+		{ disableFormats: true }
+	),
+
+	membersTemplate: new Ext.XTemplate(
+		'<div id="{[values.memberof.replace(".","_")]}-{[values.static ? "static-" : ""]}{type}-{name}" class="member">',
+			'<div class="title">',
+				'<a href="/docs/{[values.memberof.replace(".","_")]}-{type}-{name}" class="name">{name}</a>',
+				'<tpl if="type == \'method\'">',
+				'( <span class="pre">',
+				'<tpl for="params">{name}{[xindex < xcount ? ", " : ""]}</tpl>',
+				'</span> )',
+				'</tpl>',
+				'<tpl if="type == \'member\'">',
+					' : <span class="pre"><tpl for="props">',
+						'<tpl for="type">{.}{[xindex < xcount ? "|" : ""]}</tpl>',
+					'</tpl></span>',
+				'</tpl>',
+				'<tpl if="returns"> : <tpl for="returns.type">',
+					'{[this.getTypeLink(values)]}{[xindex < xcount ? "|" : ""]}',
+				'</tpl></tpl>',
+				'<tpl if="static">',
+					'<span class="signature"><span class="static">static</span></span>',
+				'</tpl>',
+			'</div>',
+			'<div class="description">',
+				'<div class="long">',
+					'{desc}',
+					'<tpl if="type == \'member\'">',
+						'<tpl for="props">',
+							'{desc}',
+						'</tpl>',
+					'</tpl>',
+					'<tpl if="params">',
+						'<h3 class="pa">Parameters</h3>',
+						'<ul><tpl for="params">',
+							'<li><span class="pre">{name}</span> : <tpl for="type">{[this.getTypeLink(values)]}{[xindex < xcount ? "|" : ""]}</tpl>',
+							'<div class="sub-desc">{desc}</div></li>',
+							'<tpl if="subparams">',
+								'<div class="sub-desc"><ul>',
+									'<tpl for="subparams">',
+										'<li><span class="pre">{name}</span> : <tpl for="type">{[this.getTypeLink(values)]}{[xindex < xcount ? "|" : ""]}</tpl><div class="sub-desc">{desc}</div></li>',
+									'</tpl>',
+								'</ul></div>',
+							'</tpl>',
+						'</tpl></ul>',
+					'</tpl>',
+					'<tpl if="returns">',
+						'<h3 class="pa">Returns</h3>',
+						'<ul><tpl for="returns.type">',
+							'<li><span class="pre">{[this.getTypeLink(values)]}</span><div class="sub-desc">{desc}</div></li>',
+						'</tpl></ul>',
+					'</tpl>',
+					'<tpl if="examples">',
+						'<h3 class="pa">Example{[values.examples.length > 1 ? "s" : ""]}</h3>',
+						'<tpl for="examples">{.}</tpl>',
 					'</tpl>',
 				'</div>',
-				'</tpl>',
-			'</div>',
-			'<div class="members-section">',
-				'<h3 class="members-title icon-method">Methods</h3>',
-				'<div class="subsection">',
-				'<tpl for="members">',
-					'<div id="{[values.memberof.replace(".","_")]}-{[values.static ? "static-" : ""]}{type}-{name}" class="member">',
-						'<div class="title">',
-							'<a href="/docs/{[values.memberof.replace(".","_")]}-{type}-{name}" class="name">{name}</a>',
-							'( <span class="pre">',
-							'<tpl for="params">{name}{[xindex < xcount ? ", " : ""]}</tpl>',
-							'</span> )',
-							'<tpl if="returns"> : <tpl for="returns.type">',
-								'{[this.getTypeLink(values)]}{[xindex < xcount ? "|" : ""]}',
-							'</tpl></tpl>',
-							'<tpl if="static">',
-								'<span class="signature"><span class="static">static</span></span>',
-							'</tpl>',
-						'</div>',
-						'<div class="description">',
-							'<div class="long">',
-								'{desc}',
-								'<tpl if="params">',
-									'<h3 class="pa">Parameters</h3>',
-									'<ul><tpl for="params">',
-										'<li><span class="pre">{name}</span> : <tpl for="type">{[this.getTypeLink(values)]}{[xindex < xcount ? "|" : ""]}</tpl>',
-										'<div class="sub-desc">{desc}</div></li>',
-										'<tpl if="subparams">',
-											'<div class="sub-desc"><ul>',
-												'<tpl for="subparams">',
-													'<li><span class="pre">{name}</span> : <tpl for="type">{[this.getTypeLink(values)]}{[xindex < xcount ? "|" : ""]}</tpl><div class="sub-desc">{desc}</div></li>',
-												'</tpl>',
-											'</ul></div>',
-										'</tpl>',
-									'</tpl></ul>',
-								'</tpl>',
-								'<tpl if="returns">',
-									'<h3 class="pa">Returns</h3>',
-									'<ul><tpl for="returns.type">',
-										'<li><span class="pre">{[this.getTypeLink(values)]}</span><div class="sub-desc">{desc}</div></li>',
-									'</tpl></ul>',
-								'</tpl>',
-								'<tpl if="examples">',
-									'<h3 class="pa">Example{[values.examples.length > 1 ? "s" : ""]}</h3>',
-									'<tpl for="examples">{.}</tpl>',
-								'</tpl>',
-							'</div>',
-						'</div>',
-					'</div>',
-				'</tpl>',
-				'</div>',
-			'</div>',
 			'</div>',
 		'</div>',
 		{
@@ -117,13 +116,16 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 	outlineTemplate: new Ext.XTemplate(
 		'<ul>',
 			'<tpl for="groups">',
-				'<tpl for="classes">',
+				'<li>{namespace}</li>',
+				'<ul>',
+				'<tpl for="members">',
 					'<li><a href="/docs/{.}">{.}</a></li>',
 				'</tpl>',
+				'</ul>',
 			'</tpl>',
 		'</ul>'
 	),
-	typedefsTemplate: new Ext.XTemplate(
+	typedefsTOCTemplate: new Ext.XTemplate(
 		'<div class="members">',
 			'<tpl for=".">',
 				'<tpl for="props">',
@@ -134,7 +136,7 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 			'</tpl>',
 		'</div>'
 	),
-	membersTemplate: new Ext.XTemplate(
+	membersTOCTemplate: new Ext.XTemplate(
 		'<div class="members">',
 			'<tpl for=".">',
 				'<div class="member">',
@@ -229,37 +231,35 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 	showDocs: function() {
 		this.getLayout().getRenderTarget().mask(this.localize('loadingDocs'));
 
-		this._loadExtOutline({
-			"name": "Spyral",
-			"groups": [
-				{
-					"name": "Base",
-					"classes": [
-						"Spyral.Corpus",
-						"Spyral.Table",
-						"Spyral.Chart",
-						"Spyral.Load",
-						"Spyral.Util",
-						"Spyral.Categories"
-					]
-				},{
-					"name": "Notebook",
-					"classes": [
-						"Spyral.Metadata",
-						"Spyral.Notebook",
-						"Spyral.Util.Storage"
-					]
-				},{
-					"name": "Global",
-					"classes": [
-						"window"
-					]
-				}
-			]
-		});
-		
-		this.getLayout().setActiveItem(0);
-		this.getLayout().getRenderTarget().unmask();
+		var docsUrl = Voyant.application.getBaseUrlFull()+'resources/spyral/inline/';
+
+		Ext.Promise.all([
+			Ext.Ajax.request({
+				url: docsUrl+'Spyral.json'
+			}),
+			Ext.Ajax.request({
+				url: docsUrl+'Tools.json'
+			})
+		]).then(function(responses) {
+			var jsons = responses.map(function(response) {
+				return JSON.parse(response.responseText)
+			});
+			this._loadExtOutline({
+				"name": "Spyral",
+				"groups": jsons.map(function(json) {
+					return {
+						"namespace": json[0].match(/^\w+/)[0],
+						"members": json
+					}
+				})
+			});
+			
+			this.getLayout().setActiveItem(0);
+			this.getLayout().getRenderTarget().unmask();
+		}.bind(this), function() {
+			this.getLayout().setActiveItem(0);
+			this.getLayout().getRenderTarget().unmask();
+		}.bind(this));
 	},
 
 	handleDocLink: function(link, rel) {
@@ -271,6 +271,7 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 				window.open(Voyant.application.getBaseUrlFull()+link.substring(1), '_spyral_docs');
 			} else {
 				console.log('unrecognized link', link, rel);
+				window.open(link, '_external');
 			}
 		} else {
 			var matches = link.match(/.*?\/docs\/([\w.]+)[#-]?(.*)?/);
@@ -283,6 +284,9 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 				} else {
 					this._showDocEntry(linkClass, linkMethod);
 				}
+			} else {
+				console.log('unrecognized link', link, rel);
+				window.open(link, '_external');
 			}
 		}
 	},
@@ -292,7 +296,7 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 		if (this.lastDocEntryClass) {
 			entryId += '/'+this.lastDocEntryClass+'.html';
 			if (this.lastDocEntryMethod) {
-				entryId += '#'+this.lastDocEntryMethod.replace('static-','.').replace('method-','').replace(/(~\w+)(-\w+)/,'$1');
+				entryId += '#'+this.lastDocEntryMethod.replace('static-','.').replace(/(method|member)-/,'').replace(/(~\w+)(-\w+)/,'$1');
 			}
 		}
 		window.open(Voyant.application.getBaseUrlFull()+'docs'+entryId, '_spyral_docs');
@@ -342,7 +346,16 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 			if (entryId) {
 				docsParentEl.querySelector('#'+entryId).hidden = false;
 			} else {
-				docsParentEl.querySelector('.doc-contents').hidden = false;
+				if (entryClass === undefined) entryClass = this.lastDocEntryClass;
+				if (entryClass.indexOf('Tools') === 0) {
+					// special Tools handling
+					this.down('#overviewBtn').hide();
+					this.down('#configsBtn').hide();
+					this.down('#methodsBtn').hide();
+					docsParentEl.querySelectorAll('.doc-contents, .members-section > .subsection > div').forEach(function(el) { el.hidden = false; });
+				} else {
+					docsParentEl.querySelector('.doc-contents').hidden = false;
+				}
 			}
 			this.getLayout().setActiveItem(0);
 			setTimeout(function() {
@@ -374,22 +387,76 @@ Ext.define('Voyant.notebook.util.DocsPanel', {
 
 		this.up().setTitle(this.localize('docs')+': '+docClass);
 
-		this._setHtmlForCard('main', this.classTemplate.apply(json));
+		this._setHtmlForCard('main', this._doClassTemplate(json));
+		this.down('#overviewBtn').show();
 
-		this._showDocEntry(docClass, docMethod);
-
-		this.down('#overviewBtn').show();		
-
-		this._setHtmlForCard('configs', this.typedefsTemplate.apply(json.typedefs));
+		this._setHtmlForCard('configs', this.typedefsTOCTemplate.apply(json.typedefs));
 		this.down('#configsBtn').setVisible(json.typedefs && json.typedefs.length > 0);
 
-		this._setHtmlForCard('methods', this.membersTemplate.apply(json.members));
+		this._setHtmlForCard('methods', this.membersTOCTemplate.apply(json.members));
 		this.down('#methodsBtn').setVisible(json.members && json.members.length > 0);
+
+		this._showDocEntry(docClass, docMethod);
 	},
 
 	_setHtmlForCard: function(cardId, html) {
 		this.down('#'+cardId).setHtml(html);
 		var cardEl = this.down('#'+cardId).getEl().dom;
 		Ext.fly(cardEl).selectable();
+	},
+
+	_doClassTemplate: function(json) {
+		var me = this;
+		var classTemplate = new Ext.XTemplate(
+			'<div>',
+				'<div class="doc-contents">{overview}</div>',
+				'<div class="members">',
+					'<div class="members-section">',
+						'<h3 class="members-title icon-method">Type Definitions</h3>',
+						'<tpl for="typedefs">',
+							'{[this.convertTypedef(values)]}',
+						'</tpl>',
+					'</div>',
+					'<div class="members-section">',
+						'<h3 class="members-title icon-method">Members</h3>',
+						'<div class="subsection">',
+						'<tpl for="members">',
+							'{[this.convertMember(values)]}',
+						'</tpl>',
+						'</div>',
+					'</div>',
+					'<div class="members-section">',
+						'<h3 class="members-title icon-method">Methods</h3>',
+						'<div class="subsection">',
+						'<tpl for="members">',
+							'{[this.convertMethod(values)]}',
+						'</tpl>',
+						'</div>',
+					'</div>',
+				'</div>',
+			'</div>',
+			{
+				disableFormats: true,
+				convertTypedef: function(json) {
+					return me.typedefsTemplate.apply(json);
+				},
+				convertMember: function(json) {
+					if (json.type === 'member') {
+						return me.membersTemplate.apply(json);
+					} else {
+						return '';
+					}
+				},
+				convertMethod: function(json) {
+					if (json.type === 'method') {
+						return me.membersTemplate.apply(json);
+					} else {
+						return '';
+					}
+				}
+			}
+		);
+
+		return classTemplate.apply(json);
 	}
 });
