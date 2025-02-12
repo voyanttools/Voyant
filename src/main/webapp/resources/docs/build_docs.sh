9a48ec6ff033f401e0cfd1a733ddc534c3a5b311
Andrew MacDonald <ajmacdonald@users.noreply.github.com>
2025-02-12 16:46:57 -0500

    chore: remove old build file

--- src/main/webapp/resources/docs/build_docs.sh
+++
@@ -1,9 +0,0 @@
-LOC1="../../../../../../voyantjs/src/"
-LOC2="../spyral/src/"
-LOC3="../../app/panel"
-OUTPUT_DIR="../../docs"
-CONFIG_FILE="config.json"
-
-npm install
-rm -r "${OUTPUT_DIR}"
-node_modules/.bin/jsdoc -c "${CONFIG_FILE}" -d "${OUTPUT_DIR}" --readme "en/guides/guides/start.md" "${LOC1}" "${LOC2}" "${LOC3}"
